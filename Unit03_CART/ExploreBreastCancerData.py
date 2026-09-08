b821c18d0ca823b65c293c2a23f29f2725bfdb6f
Nur Ceren <nurceren@ku.edu>
2026-09-07 22:29:11 -0500

    Commit my ExploreBreastCancerData

---
+++ Unit03_CART/ExploreBreastCancerData.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pansas as pd
+import math
+import scipy as sp
+from sklearn import tree
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from xgboost import XGBClassifier
+import matplotlib as mpl
+import matplotlib.pyplot as plt
