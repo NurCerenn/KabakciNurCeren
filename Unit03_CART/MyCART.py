b46a0681440d1ecf55edf0e3526c7106700a68bf
Nur Ceren <nurceren@ku.edu>
2026-09-09 12:31:54 -0500

    commiting py file MyCART up to now

--- Unit03_CART/MyCART.py
+++ Unit03_CART/MyCART.py
@@ -26,3 +26,19 @@ print(data.describe)
 print(data.shape)
 print(data.attrs)
 print(data.tail)
+
+
+#val-test data seperation
+np.random.seed(101)
+dper=data.iloc[np.random.permutation(len(data)),:]
+dval=dper.iloc[0:(math.floor(.75*len(dper))),:]
+dtest=dper.iloc[(math.floor(.75*len(dper))):(len(dper)+1),:]
+
+
+#fitting
+Xval=dval.iloc[:,1:11]
+yval=dval.diagnosis
+m_d=tree.DecisionTreeClassifier(max_depth=30,min_samples_split=20,
+min_samples_leaf=7,min_impurity_decrease=0.01,
+max_features=None,random_state=101)
+m_d.fit(Xval,yval)
